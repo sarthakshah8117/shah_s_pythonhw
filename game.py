202778aa5fcb01c62ef80096f8bd29ad87e717c7
Shah <s_shah107384@ats-hr420-dm30.local>
2019-10-22 14:44:05 -0400

    completed

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 from random import randint
 
 # choices is n array => an array is a container these   hat can hold multiple values
-	choices = ["rock", "paper" ,"scissors"]
+choices = ["rock", "paper" , "scissors"]
 
 # set a computer variable to one of these choices
 computer = choices[randint(0,2)]
@@ -11,21 +11,19 @@ player = input("choose rock, paper or scissors\n")
 # set up the game loop so that we don't have to restart all te time 
 player = False
 
-while player = False:
+while player is False:
 	#set player true
 	player = input("choose rock, paper or scissor\n")
 
 	print("computer choose:", computer, "\n")
 
 	print("player choose:", player, "\n")
-	if player== "quit":
+
+	if player == "quit":
 		exit()
-		elif("tie! no one wins, play again")
+	elif player == computer:
+		print("tie! no one wins, play again")
 
-
-
-
-
-   #need to check all of our conditions after checking for a tie
-   player = False 
-   computer = choices[randint(0, 2)]
+	#need to check all of our conditions after checking for a tie
+	player = False 
+	computer = choices[randint(0, 2)]
